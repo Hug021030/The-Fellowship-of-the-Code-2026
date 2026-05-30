81adc84385c8d82fd6fa231c55c0f3d6bd34c3fb
Artur <von18383@spengergasse.at>
2026-05-30 10:57:21 +0200

    added js bandage function

--- Artifacts/Artifact-4/src/js/script.js
+++ Artifacts/Artifact-4/src/js/script.js
@@ -1,29 +1,52 @@
 
 function useItem() {
-  alert("use works");
+  alert('Cant use this Item right now');
 }
 
+//health von member
+let health = 70;
 
+//Kann man die use Button Funktionen evtl gemeinsam generalisieren?
+//item number list so wie sie am refresh drinn sind:
+let itemsList =['4', '7', '26', '9', '420', '12', '5', '3', '2', '11', '1', '67', '5', '1', '29', '123'];
+function useHealth(){
+    //alle die die health vergrößern?
+}
 
-/*/var example;
-let itemsList =['1', '2'];
+let bandageNr = 7;
+function useBandage(){
+    if(health > 91){
+        console.log('Health über 100');
+        alert('Cant use this item, already healthy enough')
+    }
+    else if(bandageNr == 0){
+        console.log('no more bandages');
+        alert('You dont have any bandages left')
+    }
+    else {
+        health += 10;
+        bandageNr = bandageNr - 1;
+        console.log('health: ' + health + ' bandage nr: ' + bandageNr);
+        //setzen healthbar
+        document.querySelector('#healtBarText').textContent = health + '%';
+        document.getElementById('healthBarWidth').style.width = health + '%';
+        document.querySelector('#bandageNR').textContent = bandageNr + 'x';
+    }
+}
 
-window.onload=function(){
-
+//window.onload=function(){
+    
+    /*/
     document.querySelector('#jsuseItem').addEventListener('click', function () {
-    example = 1;
-    console.log('it works');
-    alert('yay');
-});
-
-
-
-    document.getElementById('js-useItem').addEventListener('click', function () {
-        console.log('use works');
-        //alert('use works hurray');
-
+        console.log('use works (query selector)');
         
     });
 
 
-}/*/
+    document.getElementById('js-useItem').addEventListener('click', function () {
+        console.log('use works (get element dy id)');
+        //alert('use works hurray');
+    });
+    /*/
+
+//} 
